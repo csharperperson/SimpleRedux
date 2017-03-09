764a60d05dfa78b08cd34d12125ed825c26fb1e5
Tyler Moore <tylermoore124@gmail.com>
2017-03-09 13:48:40 -0500

    Finished project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Redux
+
+Book list with selected book
+
+```
+	> npm install
+	> npm start
+```
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title: 'Blue Exorcist', pages: 85 },
+		{ title: 'Soul Eater', pages: 99 },
+		{ title: 'Nisekoi', pages: 102 },
+		{ title: 'Naruto', pages: 40 }
+	];
+}
